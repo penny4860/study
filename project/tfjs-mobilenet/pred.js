de795ef027510bd24bdefbef032f6682359b5284
jeongjoonsup <penny4860@gmail.com>
2019-11-26 22:21:22 +0900

    prediction 코드 정리

--- project/tfjs-mobilenet/pred.js
+++ project/tfjs-mobilenet/pred.js
@@ -7,46 +7,36 @@ const IMAGE_SIZE = 224;
 const TOPK_PREDICTIONS = 10;
 
 let mobilenet;
-async function mobilenetDemo() {
+async function predDemo() {
     // 1. load model
     mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
     mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
     
-    // 2. predict
-    const catElement = document.getElementById('cat');
-    catElement.style.display = '';
-    predict(catElement);
-}
+    // 2. image element
+    const imgElement = document.getElementById('cat');
+    imgElement.style.display = '';
 
-/**
- * Given an image element, makes a prediction through mobilenet returning the
- * probabilities of the top K classes.
- */
-async function predict(imgElement) {
+    // 3. imgElement -> pred_tensors 를 익명함수로 정의
+    const logits = tf.tidy(() => {
+        // 1) image tensor
+        let img = tf.browser.fromPixels(imgElement).toFloat();
 
-  // 1. imgElement -> pred_tensors 를 익명함수로 정의
-  const logits = tf.tidy(() => {
-    // 1. image tensor
-    let img = tf.browser.fromPixels(imgElement).toFloat();
+        // 2) preprocess
+        const offset = tf.scalar(127.5);
+        img = img.sub(offset).div(offset);
+        const imgs = img.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
 
-    // 2. preprocess
-    const offset = tf.scalar(127.5);
-    img = img.sub(offset).div(offset);
-    const imgs = img.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+        // 3) prediction
+        return mobilenet.predict(imgs);
+    });
+    
+    // 4. 익명함수를 실행해서 데이터를 뽑는다.
+    const values = await logits.data();
 
-    // 3. prediction
-    return mobilenet.predict(imgs);
-  });
-
-  // 2. 익명함수를 실행해서 데이터를 뽑는다.
-  const values = await logits.data();
-  printMsg(values.length + ": " + values[0] + ", " + values[1]);  
-}
-
-
-function printMsg(msg) {
+    // 5. 출력
     const demoStatusElement = document.getElementById('status');
-    demoStatusElement.innerText += (msg + "\n");    
+    const msg = values.length + ": " + values[0] + ", " + values[1];
+    demoStatusElement.innerText = msg;
 }
 
-mobilenetDemo();
+predDemo();
